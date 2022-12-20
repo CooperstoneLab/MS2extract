a3d262e8329aeb11360ff67c7bd56272d4e8e06e
Cristian Quiroz-Moreno <cristianquirozd1997@gmail.com>
2022-12-20 17:01:47 -0500

    ppm utils
    Create two functions to calculate the ppm error and the ppm error ranges

---
+++ R/get_ppm.R
@@ -0,0 +1,51 @@
+#' Calculate the m/z difference for a given m/z and ppm tolerance
+#'
+#' Given a theretical m/z value and ppm tolerance, computes the m/z
+#' deviation, or the observed m/z value
+#' for the given theoretical m/z value.
+#'
+#' @param mz a double, the theoretical m/z value.
+#' @param ppm a double, the ppm error tolerance.
+#'
+#' @return a double with the m/z value deviation from the given
+#' theorical m/z value
+#'
+#' @export
+#'
+#' @examples
+#' get_ppm(mz = 355.1023, ppm = 20)
+#'
+#' # Chlorogenic acid [M+H]+ = 355.1023 m/z
+#' chlorogenic_acid_pos <- 355.1023
+#' ppm_error = 10
+#' get_ppm(mz = chlorogenic_acid_pos, ppm = ppm_error)
+
+get_ppm <- function(mz, ppm = 10) {
+  mz_difference <- ( ppm * mz / 1E6) + mz
+  mz_difference
+}
+
+
+
+#' Calculate the m/z upper and lower values limits given a ppm tolerance
+#'
+#' Given a theoretical m/z value and a ppm error tolerance,
+#' this function calculates the lower and upper m/z values.
+#'
+#' @param mz a double, the theoretical m/z value.
+#' @param ppm a double, the ppm error tolerance
+#'
+#' @return a vector of two elements containing the upper and lower
+#' m/z values with the specific ppm error tolerance.
+#'
+#' @export
+#'
+#' @examples
+#' # Chlorogenic acid [M+H]+ = 355.1023 m/z
+#' chlorogenic_acid_pos <- 355.1023
+#' ppm_error = 10
+#' ppm_range(mz = chlorogenic_acid_pos, ppm = ppm_error)
+ppm_range <- function(mz, ppm) {
+  ppm_error <- get_ppm(mz = mz, ppm = ppm)
+  ppm_range_value <- c(mz - ppm_error, mz + ppm_error)
+}
