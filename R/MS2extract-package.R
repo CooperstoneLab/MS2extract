4869b061510d3781f4f070a98cf8991825200d41
Cristian Quiroz-Moreno <cristianquirozd1997@gmail.com>
2023-01-19 13:13:39 -0500

    Add ms2_plot
    Function to plot MS2 spectra while extracting the most intense scan

--- R/MS2extract-package.R
+++ R/MS2extract-package.R
@@ -2,6 +2,7 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom dplyr arrange
 #' @importFrom dplyr bind_rows
 #' @importFrom dplyr mutate
 #' @importFrom dplyr select
@@ -12,9 +13,13 @@
 #' @importFrom ggplot2 ggplot
 #' @importFrom ggplot2 labs
 #' @importFrom ggplot2 theme_bw
+#' @importFrom ggplot2 theme_classic
+#' @importFrom ggpubr ggarrange
+#' @importFrom ggrepel geom_label_repel
 #' @importFrom magrittr %>%
 #' @importFrom MSnbase readMSData
 #' @importFrom OrgMassSpecR SpectrumSimilarity
+#' @importFrom patchwork /
 #' @importFrom ProtGenerics spectra
 #' @importFrom purrr map
 #' @importFrom rlang .data
--- R/extract_MS2.R
+++ R/extract_MS2.R
@@ -25,7 +25,7 @@ plot_tic <- function(TIC) {
                               aes(x = .data$rt, y = .data$TIC)) +
     ggplot2::geom_line() + ggplot2::geom_point(size = 2) +
     ggplot2::geom_point(data = most_intense, color = "red", size = 3) +
-    ggplot2::labs(title = "TIC plot",
+    ggplot2::labs(title = "MS2 TIC plot",
                   subtitle = paste0("MS2 spectra at ","rt: ",
                                     most_intense$rt,
                                     " will be exported"),
@@ -34,6 +34,44 @@ plot_tic <- function(TIC) {
 
   return_list <- list(most_intense = most_intense, TIC_plot = TIC_plot)
   return(return_list)
+}
+
+#' Plot MS2 spectra
+#'
+#' This functions plot the resulting MS2 spectra of the most intense scan
+#'
+#' @param spec a data frame containing the MS2 spectra of the most intense
+#' scan
+#' @param max_labels an integer indicating the maximum number of labels of
+#' the most intense ions
+#' @return a ggplot plot
+
+plot_MS2spectra <- function(spec, max_labels = 5) {
+
+  if(nrow(spec) > 4){
+    spec_nrow <- nrow(spec)
+    most_inte_label <- dplyr::arrange(spec, - .data$intensity) |>
+      dplyr::mutate(intense = seq(dplyr::n()) ) |>
+      dplyr::filter(.data$intense < max_labels )
+  } else {
+    most_inte_label <- spec
+  }
+
+  most_inte_label <- most_inte_label |>
+    dplyr::mutate(mz = round(.data$mz, 4))
+
+  ms2_spec <- ggplot2::ggplot(spec,
+                              aes(.data$mz, .data$intensity)) +
+    ggplot2::geom_col(width = 1) +
+    ggplot2::geom_point(data = most_inte_label, color = "red") +
+    ggplot2::theme_classic() +
+    ggrepel::geom_label_repel(data = most_inte_label,
+                              aes(label = .data$mz),
+                              box.padding = 0.5,
+                              max.overlaps = Inf)
+
+
+  return(ms2_spec)
 }
 
 #' Extrac the most MS2 intense scan
@@ -47,6 +85,7 @@ plot_tic <- function(TIC) {
 #' @param out_list a boolean expresing if the output is a list containing
 #' the MS2 spectra plus the TIC chromatogram (verbose = TRUE), or only
 #' the data frame with the MS2 spectra (verbose = FALSE).
+#' @param  ... any other arguments passed to detect_mass() function
 #' @export
 #' @examples
 #' # Importing the Spectrum of Procyanidin A2 in negative ionzation mode
@@ -64,19 +103,28 @@ plot_tic <- function(TIC) {
 #' # Returning MS2 spectra only
 #' extract_MS2(ProcA2_raw, out_list = FALSE)
 
-extract_MS2 <- function(spec, verbose = TRUE, out_list = TRUE) {
-  TIC <- get_TIC(spec)
+extract_MS2 <- function(spec, verbose = TRUE, out_list = TRUE, ...) {
 
+  # Get MS2 TIC
+  TIC <- get_TIC(spec)
+  # Plot MS2 TIC
   TIC_results <- plot_tic(TIC)
 
-  if(verbose) print(TIC_results$TIC_plot)
-
+  # Calculate the most intense scan
   most_intense <- TIC_results$most_intense
 
+  # Get the rt of the most intense scan
   MS2_spec <- dplyr::filter(spec, .data$rt == most_intense$rt)
+  spec_plot <- plot_MS2spectra(MS2_spec)
 
+
+  plot_results <- ggpubr::ggarrange(TIC_results$TIC_plot, spec_plot, ncol = 1)
+  # IF verbose = T print the plot
+  if(verbose) print(plot_results)
+
+  # Modify what is returned
   if(out_list){
-    return( list(MS2_spec = MS2_spec, TIC_plot = TIC_results$TIC_plot) )
+    return( list(MS2_spec = MS2_spec, TIC_plot = plot_results) )
   } else {
       return(MS2_spec)
     }
