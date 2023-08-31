a5ab032f286dcf3c7e69c5fa3b75c627cacc208c
Cristian Quiroz-Moreno <cristianquirozd1997@gmail.com>
2023-08-31 16:11:33 -0400

    Ungrouping for compare_spectra

--- R/compare_spectra.R
+++ R/compare_spectra.R
@@ -50,9 +50,11 @@
 #' compare_spectra(ProcA2_norm, ProcA2_PCDL)
 compare_spectra <- function(spec1, spec2, output.list = TRUE, ...) {
   # select mz and intensity columns from spec1
+  spec1 <- dplyr::ungroup(spec1)
   spec1 <- dplyr::select(spec1, .data$mz, .data$intensity)
 
   # select mz and intensity columns from spec2
+  spec2 <- dplyr::ungroup(spec2)
   spec2 <- dplyr::select(spec2, .data$mz, .data$intensity)
 
 
